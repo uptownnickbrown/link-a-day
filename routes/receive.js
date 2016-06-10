0a1eae8e6e51ce4964485065c5f722ec976d2d9e
Nick Brown <nicholas.tyler.brown@gmail.com>
2016-06-10 00:29:23 -0400

    Logging on heroic

--- routes/receive.js
+++ routes/receive.js
@@ -3,7 +3,10 @@ var router = express.Router();
 
 /* POST email reception. */
 router.post('/', function(req, res, next) {
-  res.send(req, res);
+  console.log('processing POSTed email');
+  console.log(req);
+  console.log(res);
+  res.send('OK');
 });
 
 module.exports = router;
