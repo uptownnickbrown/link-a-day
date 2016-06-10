8e54113cf26d5ab117d618d90229897cbdef2fe5
Nick Brown <nicholas.tyler.brown@gmail.com>
2016-06-10 00:48:09 -0400

    Auto-respond when receiving posts

--- routes/receive.js
+++ routes/receive.js
@@ -1,11 +1,25 @@
 var express = require('express');
 var router = express.Router();
 
+var api_key = 'key-b9f6ce56812f37467732aaf3097f88f7';
+var domain = 'mg.quanticle.co';
+var mailgun = require('mailgun-js')({apiKey: api_key, domain: domain});
+
 /* POST email reception. */
 router.post('/', function(req, res, next) {
   console.log('processing POSTed email');
   console.log(req);
-  console.log(res);
+
+  var data = {
+    from: 'Link-a-Day <link-a-day@mg.quanticle.co>',
+    to: 'nicholas.tyler.brown+link-a-day@gmail.com',
+    subject: 'Link-a-Day',
+    text: req
+  };
+  mailgun.messages().send(data, function (error, body) {
+    res.send(body);
+  });
+
   res.send('OK');
 });
 
