eca337db930376eaa6a5efc00a7a2ec6a680d542
Nick Brown <nicholas.tyler.brown@gmail.com>
2016-06-10 22:17:18 -0400

    added subject and body

--- routes/receive.js
+++ routes/receive.js
@@ -3,20 +3,25 @@ var router = express.Router();
 
 var api_key = 'key-b9f6ce56812f37467732aaf3097f88f7';
 var domain = 'mg.quanticle.co';
-var mailgun = require('mailgun-js')({apiKey: api_key, domain: domain});
+
 
 /* POST email reception. */
 router.post('/', function(req, res) {
-  console.log(req);
-  console.log(req.body);
-  console.log(req.body.message);
+  var mailgunBody = req.body;
+  var messageSubject = mailgunBody['subject'];
+  var messageBody = mailgunBody['body-plain'];
   var data = {
     from: 'Link-a-Day <link-a-day@mg.quanticle.co>',
     to: 'nicholas.tyler.brown+link-a-day@gmail.com',
-    subject: 'Link-a-Day',
-    text: req.body.message
+    subject: messageSubject,
+    text: messageBody
   };
+  console.log(data);
+  var mailgun = require('mailgun-js')({apiKey: api_key, domain: domain});
+  console.log('initialized mailgun');
   mailgun.messages().send(data, function (error, body) {
+    console.log('attempted to send message');
+    console.log(body);
     res.send('OK');
   });
 });
