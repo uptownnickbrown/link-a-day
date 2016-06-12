46ce35c37eef903983f25e0cf5428cf7602d343e
Nick Brown <nicholas.tyler.brown@gmail.com>
2016-06-11 22:30:48 -0400

    Switch to Cheerio, better bad link error handling and logging

--- routes/submit.js
+++ routes/submit.js
@@ -1,7 +1,8 @@
 var express = require('express');
 var router = express.Router();
 var firebase = require('firebase');
-var MetaInspector = require('node-metainspector');
+var cheerio = require('cheerio');
+var request = require('request');
 var validUrl = require('valid-url');
 
 var api_key = 'key-b9f6ce56812f37467732aaf3097f88f7'; // TODO gen new API key, get this out of git
@@ -41,16 +42,26 @@ var addToLinkQueue = function(recommendation,recommenderId) {
   var newLinkRef = linksRef.push();
   var recommendationTitle = recommendation.url;
 
-  var client = new MetaInspector(recommendation.url, {timeout:15000});
-  client.on("fetch", function(){
-    newLinkRef.set({
+  request(recommendation.url, function (error, response, html) {
+    if (!error && response.statusCode == 200) {
+      var $ = cheerio.load(html,{ normalizeWhitespace: true, decodeEntities: true });
+      var title = $('title').text();
+      recommendationTitle = title;
+    }
+    else {
+      console.log("Error parsing URL: " + response.statusCode);
+    }
+    var newLink = {
       recommenderId: recommenderId,
       url: recommendation.url,
-      title: client.title || recommendation.url,
+      title: recommendationTitle,
       blurb: recommendation.blurb
-    });
+    };
+    console.log('adding new link to queue');
+    console.log(newLink);
+    newLinkRef.set(newLink);
   });
-  client.fetch();
+
   return newLinkRef.key;
 };
 
@@ -83,6 +94,7 @@ router.post('/', function(req, res) {
 
   // Is this a valid URL?
   if (validUrl.isWebUri(recommendation.url)) {
+    console.log('Valid URL, entering the main loop');
     // check to see if we've seen this recommender before
     usersRef.orderByChild("email").equalTo(recommendation.submitterEmail.toLowerCase()).once('value',function(snapshot){
       var user = snapshot.val();
@@ -96,13 +108,12 @@ router.post('/', function(req, res) {
         recommenderId = newUserKey;
         addToLinkQueue(recommendation,newUserKey);
       }
-
       var newRecommendation = retrieveFromLinkQueue(recommenderId,function(newRecommendation) {
         var recommendationRef = db.ref("links/" + newRecommendation);
         recommendationRef.once('value',function(snapshot){
           var outboundRecommendation = snapshot.val();
           var outboundBody = 'Thanks for submitting ' + recommendation.url + ' to Link-a-Day. We\'ll get that shared with someone soon and let you know if they want to chat.\n\nIn the meantime, take a look at this great recommendation "' + outboundRecommendation.title + '":\n' + outboundRecommendation.url + '\n\nSomeone loved this link, saying "' + outboundRecommendation.blurb + '"\n\nWant to talk more with them about it? Reply to this email and we\'ll connect you right away.\nThanks - Link-a-Day';
-
+          console.log('Got a good recommendation, sending out a new one.')
           mailgun.messages().send({
             from: 'Link-a-Day <link-a-day@mg.quanticle.co>',
             to: recommendation.submitterEmail,
@@ -119,12 +130,15 @@ router.post('/', function(req, res) {
               replied: 0
             };
             newConnectionRef.set(connection);
+            console.log('Added the connection.')
+            console.log(connection);
             res.send(body);
           });
         });
       });
     });
   } else {
+    console.log('Hit a problem - sending out the failure email');
     // Not a valid URL - send them a reply to re-submit with the proper formatting
     mailgun.messages().send({
       from: 'Link-a-Day <link-a-day@mg.quanticle.co>',
